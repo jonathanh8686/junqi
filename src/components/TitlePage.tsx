191ac574b60ddf200c3a2405dcec6f720f093bbe
Jonathan Hsieh <jonathanh1386@gmail.com>
2022-08-15 15:57:25 -0700

    Remove boilerplate code

---
+++ src/components/TitlePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface ITitleProps  {
+
+};
+
+export default function TitlePage(props: ITitleProps) {
+  return (
+    <div>
+        <h1>Junqi</h1>
+    </div>
+  )
+}
