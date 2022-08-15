51b6f70c28ff5e0c4fc1d271b06c0e168f2548bd
Jonathan Hsieh <jonathanh1386@gmail.com>
2022-08-15 16:56:22 -0700

    moved socket connection code to app

--- src/components/TitlePage.tsx
+++ src/components/TitlePage.tsx
@@ -7,19 +7,7 @@ interface ITitleProps  {
 
 export default function TitlePage(props: ITitleProps) {
 
-  const connectSocket = async () => {
-    if(!process.env.REACT_APP_SERVER_URL) {
-      console.warn("Could not find server url in config");
-      return;
-    }
-    const socket = await socketService.connect(process.env.REACT_APP_SERVER_URL).catch((err) => {
-      console.log("Error: ", err);
-    });
 
-  }
-  useEffect(() => {
-    connectSocket();
-  }, []);
 
   return (
     <div>
